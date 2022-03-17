9a33571fbad5cd828d120757b8be624e0b709d76
Cristian Eduardo <cristianeduardo.dev@gmail.com>
2022-03-16 23:46:03 -0300

    EJS template and Google Auth

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.set('view engine', 'ejs');
 
-app.get('/', (request: Request, response: Response) => {
-  return response.status(200).json({ oi: 'eai' });
-});
+app.use(express.static('public'));
 
-app.listen(3333, () => console.log('server started'));
+app.get('/', (request, response) =>
+  response.render('home', { CLIENT_ID: process.env.CLIENT_ID }),
+);
+
+app.listen(3333, () =>
+  console.log(`server started in http://localhost:${process.env.PORT}`),
+);
