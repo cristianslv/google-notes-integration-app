00833b1e1054f940f398f40a369baf22ff45660f
Cristian Eduardo <cristianeduardo.dev@gmail.com>
2022-03-23 01:14:34 -0300

    Adding todos

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-let userAccessToken;
 const GOOGLE_CLIENT_ID = process.env.CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.SECRET_CLIENT_ID;
 
-export { userAccessToken, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET };
+export { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET };
